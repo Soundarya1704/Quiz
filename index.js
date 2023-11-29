b0cd79099864ac8849318a2eec6af0967ad4c259
Soundarya1704 <148459663+Soundarya1704@users.noreply.github.com>
2023-11-29 20:10:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,191 @@
+const quizData = [
+    {
+      question: 'What is the capital of India?',
+      options: ['Lucknow', 'Karnataka', 'New Delhi', 'Bihar'],
+      answer: 'New Delhi',
+    },
+    {
+      question: 'What is the National Animal of India?',
+      options: ['Lion', 'Tiger', 'Deer', 'Elephant'],
+      answer: 'Tiger',
+    },
+    {
+      question: 'What is the National Fruit of India?',
+      options: ['Apple', 'Dragon Fruit', 'Bananna', 'Mango'],
+      answer: 'Mango',
+    },
+    {
+      question: 'What is the National Tree of India?',
+      options: ['Banyan Tree', 'Neem Tree', 'Pine Tree', 'Ashoka Tree'],
+      answer: 'Banyan Tree',
+    },
+    {
+      question: 'India has how many union Territories?',
+      options: [
+        '9',
+        '7',
+        '8',
+        '6',
+      ],
+      answer: '8',
+    },
+    {
+      question: 'What is the National Bird of India?',
+      options: ['Parrot', 'Pigeon', 'Peacock', 'Crow'],
+      answer: 'Peacock',
+    },
+    {
+      question: 'What is the National Flower of India?',
+      options: [
+        'Jasmin',
+        'Lotus',
+        'Rose',
+        'Lily',
+      ],
+      answer: 'Lotus',
+    },
+    {
+      question: 'How many Asiatic Lions are present on the Indian State Emblem?',
+      options: ['2', '4', '3', '6'],
+      answer: '4',
+    },
+    {
+      question: 'What is the National Currency of India?',
+      options: [
+        'Dollar',
+        'Yen',
+        'Rupees',
+        'Euros',
+      ],
+      answer: 'Rupees',
+    },
+    {
+      question: 'Which animal is known as the King of the Jungle?',
+      options: ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+      answer: 'Lion',
+    },
+  ];
+  
+  const quizContainer = document.getElementById('quiz');
+  const resultContainer = document.getElementById('result');
+  const submitButton = document.getElementById('submit');
+  const retryButton = document.getElementById('retry');
+  const showAnswerButton = document.getElementById('showAnswer');
+  
+  let currentQuestion = 0;
+  let score = 0;
+  let incorrectAnswers = [];
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
+  function displayQuestion() {
+    const questionData = quizData[currentQuestion];
+  
+    const questionElement = document.createElement('div');
+    questionElement.className = 'question';
+    questionElement.innerHTML = questionData.question;
+  
+    const optionsElement = document.createElement('div');
+    optionsElement.className = 'options';
+  
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+  
+    for (let i = 0; i < shuffledOptions.length; i++) {
+      const option = document.createElement('label');
+      option.className = 'option';
+  
+      const radio = document.createElement('input');
+      radio.type = 'radio';
+      radio.name = 'quiz';
+      radio.value = shuffledOptions[i];
+  
+      const optionText = document.createTextNode(shuffledOptions[i]);
+  
+      option.appendChild(radio);
+      option.appendChild(optionText);
+      optionsElement.appendChild(option);
+    }
+  
+    quizContainer.innerHTML = '';
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    if (selectedOption) {
+      const answer = selectedOption.value;
+      if (answer === quizData[currentQuestion].answer) {
+        score++;
+      } else {
+        incorrectAnswers.push({
+          question: quizData[currentQuestion].question,
+          incorrectAnswer: answer,
+          correctAnswer: quizData[currentQuestion].answer,
+        });
+      }
+      currentQuestion++;
+      selectedOption.checked = false;
+      if (currentQuestion < quizData.length) {
+        displayQuestion();
+      } else {
+        displayResult();
+      }
+    }
+  }
+  
+  function displayResult() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'inline-block';
+    resultContainer.innerHTML = `You scored ${score} out of ${quizData.length}!`;
+  }
+  
+  function retryQuiz() {
+    currentQuestion = 0;
+    score = 0;
+    incorrectAnswers = [];
+    quizContainer.style.display = 'block';
+    submitButton.style.display = 'inline-block';
+    retryButton.style.display = 'none';
+    showAnswerButton.style.display = 'none';
+    resultContainer.innerHTML = '';
+    displayQuestion();
+  }
+  
+  function showAnswer() {
+    quizContainer.style.display = 'none';
+    submitButton.style.display = 'none';
+    retryButton.style.display = 'inline-block';
+    showAnswerButton.style.display = 'none';
+  
+    let incorrectAnswersHtml = '';
+    for (let i = 0; i < incorrectAnswers.length; i++) {
+      incorrectAnswersHtml += `
+        <p>
+          <strong>Question:</strong> ${incorrectAnswers[i].question}<br>
+          <strong>Your Answer:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+          <strong>Correct Answer:</strong> ${incorrectAnswers[i].correctAnswer}
+        </p>
+      `;
+    }
+  
+    resultContainer.innerHTML = `
+      <p>You scored ${score} out of ${quizData.length}!</p>
+      <p>Incorrect Answers:</p>
+      ${incorrectAnswersHtml}
+    `;
+  }
+  
+  submitButton.addEventListener('click', checkAnswer);
+  retryButton.addEventListener('click', retryQuiz);
+  showAnswerButton.addEventListener('click', showAnswer);
+  
+  displayQuestion();
